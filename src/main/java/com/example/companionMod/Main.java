dba125e5108ed471a78585145855da6fbb78fbfe
g-raj <116694012+Gaut-raj@users.noreply.github.com>
2026-03-31 13:44:31 -0400

    Updated main file

--- src/main/java/com/example/companionMod/Main.java
+++ src/main/java/com/example/companionMod/Main.java
@@ -11,7 +11,16 @@ public class Main implements ModInitializer {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
+        public static final Logger LOGGER = LogManager.getLogger();
+        public static final String MOD_ID = "companionmod";
 
+        public CompanionMod() {
+            IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+            EntiryInit.register(eventBus);
+            ItemInit.register(eventBus);
+            
+        }
         ModItems.registerModItems();
 
         System.out.println("Companion Mod initialized!");
